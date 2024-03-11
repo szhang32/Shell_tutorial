0d48cd8784cb00500553ec88a610c98d8a205575
szhang32 <shuozhang23@gmail.com>
2024-03-11 00:40:05 -0400

    Update main_interactive.sh

--- scripts/main_interactive.sh
+++ scripts/main_interactive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ..
 module add bowtie2/2.3.4.3
-bowtie2 -x ref/ecoli -1 raw_fastq/SRR8383505_1M_R1.fastq.gz -2 raw_fastq/SRR8383505_1M_R2.fastq.gz -S ecoli_alignment
+bowtie2 -x ref/ecoli -1 raw_fastq/SRR8383505_1M_R1.fastq.gz -2 raw_fastq/SRR8383505_1M_R2.fastq.gz -S ecoli_alignment.sam
